a6c9509cdb0d4bd9114f00b46254c53bf77e2705
Khang Nguyen <khang8131398@gmail.com>
2020-06-19 01:38:59 +0700

    capnhat

---
+++ Bigschool/DTOS/AttendanceDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bigschool.DTOS
+{
+    public class AttendanceDto
+    {
+        
+            public int CourseId { get; set; }
+        
+    }
+}
